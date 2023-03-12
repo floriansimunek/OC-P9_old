0e9fb02bc5e4565eec82f57fbec60965f6808a7b
Florian SIMUNEK <florian.simunek@gmail.com>
2023-03-12 21:22:37 +0100

    :white_check_mark: Add window icon test

--- frontend/src/__tests__/Bills.js
+++ frontend/src/__tests__/Bills.js
@@ -13,27 +13,25 @@ import { localStorageMock } from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
-	beforeEach(() => {
-		Object.defineProperty(window, "localStorage", { value: localStorageMock });
-		window.localStorage.setItem(
-			"user",
-			JSON.stringify({
-				type: "Employee",
-			}),
-		);
-		const root = document.createElement("div");
-		root.setAttribute("id", "root");
-		document.body.append(root);
-		router();
-		window.onNavigate(ROUTES_PATH.Bills);
-	});
-
 	describe("When I am on Bills Page", () => {
 		test("Then bill icon in vertical layout should be highlighted", async () => {
+			Object.defineProperty(window, "localStorage", { value: localStorageMock });
+			window.localStorage.setItem(
+				"user",
+				JSON.stringify({
+					type: "Employee",
+				}),
+			);
+			const root = document.createElement("div");
+			root.setAttribute("id", "root");
+			document.body.append(root);
+			router();
+			window.onNavigate(ROUTES_PATH.Bills);
 			await waitFor(() => screen.getByTestId("icon-window"));
 			const windowIcon = screen.getByTestId("icon-window");
-			//to-do write expect expression
+			expect(windowIcon.classList.contains("active-icon")).toBe(true);
 		});
+
 		test("Then bills should be ordered from earliest to latest", () => {
 			document.body.innerHTML = BillsUI({ data: bills });
 			const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map((a) => a.innerHTML);
@@ -44,6 +42,18 @@ describe("Given I am connected as an employee", () => {
 
 		describe("When I click on eye icon", () => {
 			test("Then modal should be open", () => {
+				Object.defineProperty(window, "localStorage", { value: localStorageMock });
+				window.localStorage.setItem(
+					"user",
+					JSON.stringify({
+						type: "Employee",
+					}),
+				);
+				const root = document.createElement("div");
+				root.setAttribute("id", "root");
+				document.body.append(root);
+				router();
+				window.onNavigate(ROUTES_PATH.Bills);
 				const billsContainer = new Bills({
 					document,
 					onNavigate,
@@ -63,6 +73,18 @@ describe("Given I am connected as an employee", () => {
 
 		describe("When I click on the new bill button 'Nouvelle note de frais'", () => {
 			test("Then I should be redirected to 'new bill' page", () => {
+				Object.defineProperty(window, "localStorage", { value: localStorageMock });
+				window.localStorage.setItem(
+					"user",
+					JSON.stringify({
+						type: "Employee",
+					}),
+				);
+				const root = document.createElement("div");
+				root.setAttribute("id", "root");
+				document.body.append(root);
+				router();
+				window.onNavigate(ROUTES_PATH.Bills);
 				const billsContainer = new Bills({
 					document,
 					onNavigate,
